a5e312817a963260b04a44388947f4058d3395e0
Luthfi Rais Asral <mrluthfirais@gmail.com>
2021-10-01 08:53:54 +0700

    Instagram : @rais.hub

---
+++ HelloWorld1.java
@@ -0,0 +1,8 @@
+public class HelloWorld1 {
+
+	public static void main(String[] args){
+		System.out.println("Nama : Luthfi Rais Asral");
+		System.out.println("NIM : 6706210132");
+		System.out.println("Kelas : D3RPLA-45-02");
+	}
+}
---
+++ var2.java
@@ -0,0 +1,12 @@
+public class var2 {
+
+	public static void main(String[] args){
+		double Ipk = 3.57;
+		String Nama = "Luthfi Rais Asral";
+		long Nim = 6706210132L;
+		
+		System.out.println("Nama : " + Nama);
+		System.out.println("NIM : " + Nim);
+		System.out.println("IPK : " + Ipk);
+	}
+}
